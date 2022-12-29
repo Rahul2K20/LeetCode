8fdad67059b6f58e156c96aada7d5c1176470f16
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2022-12-28 20:44:17 -0800

    Changes

--- Easy/Move Zeros.py
+++ Easy/Move Zeros.py
@@ -5,3 +5,10 @@ class Solution(object):
            if nums[i] != 0: 
                nums[i],nums[j] = nums[j], nums[i]
                j+=1
+               
+               
+# We use i to keep track of position of the first zero in the list (which changes as we go).
+# We usejto keep track of the first non-zero value after the first zero (which is pointed by i).
+# Each time we have i correctly points to a zero and j correctly points to the first non-zero after i, we swap the values that store at i and j.
+# By doing this, we move zeros towards the end of the list gradually until j reaches the end.
+# And when it does, we are done.

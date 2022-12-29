9be0e89bd2b4c2091dd760de6971f72739a21f69
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2022-12-28 20:46:51 -0800

    Added comments

--- Easy/Move Zeros.py
+++ Easy/Move Zeros.py
@@ -8,7 +8,7 @@ class Solution(object):
                
                
 # We use i to keep track of position of the first zero in the list (which changes as we go).
-# We usejto keep track of the first non-zero value after the first zero (which is pointed by i).
+# We use j to keep track of the first non-zero value after the first zero (which is pointed by i).
 # Each time we have i correctly points to a zero and j correctly points to the first non-zero after i, we swap the values that store at i and j.
 # By doing this, we move zeros towards the end of the list gradually until j reaches the end.
 # And when it does, we are done.

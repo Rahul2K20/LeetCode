686e9c5244a022fc75a2d4b81bb329b0d546e8e2
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2023-01-06 04:27:39 -0800

    Solved using recursion

---
+++ Easy/Binary Tree Preorder Traversal.py
@@ -0,0 +1,6 @@
+#preorder: Root, left, Right 
+class Solution(object):
+    def preorderTraversal(self, root):
+        if not root: 
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right
